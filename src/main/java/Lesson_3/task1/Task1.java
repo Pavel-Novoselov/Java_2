90a9a93adcac8ab55027c59fab1787102b118bcb
Pavel-Novoselov <npe@inbox.ru>
2019-10-28 13:09:17 +0300

    end of less3

--- src/main/java/Lesson_3/task1/Task1.java
+++ src/main/java/Lesson_3/task1/Task1.java
@@ -1,19 +1,31 @@
 package main.java.Lesson_3.task1;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) throws IOException {
   //      File file = new File("D:/Dropbox/GeekBrain/Java_2/111/test1.txt");
-        FileInputStream fileInputStream = new FileInputStream("D:/Dropbox/GeekBrain/Java_2/111/test3.txt");
+  //      FileInputStream fileInputStream = new FileInputStream("D:/Dropbox/GeekBrain/Java_2/111/test3.txt");
+        BufferedInputStream in =  new BufferedInputStream(new FileInputStream("D:/Dropbox/GeekBrain/Java_2/111/test3.txt"));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        byte[] arr = new byte[50];
+//        byte[] arr = new byte[50];
         int x;
-        while ((x = fileInputStream.read(arr)) != -1) {
-                System.out.println(new String(arr,0, x));
-            System.out.println("Количество байт: "+x);
-            }
-    }
+ //       while ((x = fileInputStream.read(arr)) != -1) {
+ //               System.out.println(new String(arr,0, x));
+ //           System.out.println("Количество байт: "+x);
+ //           }
 
+        while((x=in.read())!=-1) out.write(x);
+
+        byte[] b = out.toByteArray();
+        System.out.println(Arrays.toString(b));
+        in.close();
+        out.close();
+        System.out.println(new String(b,0,b.length));
+    }
 }
--- src/main/java/Lesson_3/task3/Task3.java
+++ src/main/java/Lesson_3/task3/Task3.java
@@ -7,56 +7,70 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(new File("D:/Dropbox/GeekBrain/Java_2/111/test1.txt"));
-        ArrayList<char[]> strings = new ArrayList<>();
-        ArrayList<Character> text =new ArrayList<>();
+//        Scanner sc = new Scanner(new File("D:/Dropbox/GeekBrain/Java_2/111/test1.txt"));
+//        ArrayList<char[]> strings = new ArrayList<>();
+//        ArrayList<Character> text =new ArrayList<>();
+//
+//        int countChar=0;
+//        final int charInPage = 1800;
+//
+//        long t= System.currentTimeMillis();
+////записываем аррэйлист считанные строки
+//        while(sc.hasNext()){
+//            char[] temp = sc.nextLine().toCharArray();
+//            strings.add(temp);
+//            countChar+=temp.length;
+//        }
+////записываем все строки в ОДИН массив символов с добавлением переноса после каждой строки
+//        for (char[] cA:
+//                strings) {
+//            for (int i = 0; i < cA.length; i++) {
+//                text.add(cA[i]);
+//            }
+//            String s = "\n";
+//            text.add(s.charAt(0));
+//        }
+////вывод: время и кол-во символов
+//        System.out.println("время загрузки: "+(System.currentTimeMillis()-t));
+//        System.out.println("кол-во символов в киге: "+countChar);
+//
+//        int page=0;
+//        boolean end=false; //флаг окончания файла
+//
+//        Scanner sc1 = new Scanner(System.in);
+////вывод запрошенной страницы
+//        do{
+//            System.out.println("\n Page number? ");
+//            String str1=sc1.next();
+//            page=Integer.parseInt(str1);
+//
+//            long t1 = System.currentTimeMillis();
+//
+//            for (int i = (page-1)*charInPage; i <(page*charInPage) ; i++) {
+//                try{
+//                    System.out.print(text.get(i));
+//                } catch (IndexOutOfBoundsException e){
+//                    System.out.println("\n Конец файла!");
+//                    end=true;
+//                    break;
+//                }
+//            }
+//            System.out.println("\n время вывода страницы: "+(System.currentTimeMillis()-t1));
+//            if (end) break;
+//        } while (true);
 
-        int countChar=0;
-        final int charInPage = 1800;
+        final int PAGE_SIZE = 1800;
+        RandomAccessFile raf = new RandomAccessFile("D:/Dropbox/GeekBrain/Java_2/111/test1.txt", "r");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Page number? ");
+        int p = sc.nextInt()-1;
 
-        long t= System.currentTimeMillis();
-//записываем аррэйлист считанные строки
-        while(sc.hasNext()){
-            char[] temp = sc.nextLine().toCharArray();
-            strings.add(temp);
-            countChar+=temp.length;
-        }
-//записываем все строки в ОДИН массив символов с добавлением переноса после каждой строки
-        for (char[] cA:
-                strings) {
-            for (int i = 0; i < cA.length; i++) {
-                text.add(cA[i]);
-            }
-            String s = "\n";
-            text.add(s.charAt(0));
-        }
-//вывод: время и кол-во символов
-        System.out.println("время загрузки: "+(System.currentTimeMillis()-t));
-        System.out.println("кол-во символов в киге: "+countChar);
+        raf.seek(p*PAGE_SIZE);
+        byte [] bArr = new byte[1800];
+        raf.read(bArr);
+        System.out.println(new String(bArr));
+        raf.close();
 
-        int page=0;
-        boolean end=false; //флаг окончания файла
 
-        Scanner sc1 = new Scanner(System.in);
-//вывод запрошенной страницы
-        do{
-            System.out.println("\n Page number? ");
-            String str1=sc1.next();
-            page=Integer.parseInt(str1);
-
-            long t1 = System.currentTimeMillis();
-
-            for (int i = (page-1)*charInPage; i <(page*charInPage) ; i++) {
-                try{
-                    System.out.print(text.get(i));
-                } catch (IndexOutOfBoundsException e){
-                    System.out.println("\n Конец файла!");
-                    end=true;
-                    break;
-                }
-            }
-            System.out.println("\n время вывода страницы: "+(System.currentTimeMillis()-t1));
-            if (end) break;
-        } while (true);
     }
 }
