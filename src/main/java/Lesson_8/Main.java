48cb6ead1615006ac782e704b341c4db8472592f
Pavel-Novoselov <npe@inbox.ru>
2019-11-11 01:12:44 +0300

    Финальный вариант ДЗ урок 8 - прошлые не смотреть)

--- src/main/java/Lesson_8/Main.java
+++ src/main/java/Lesson_8/Main.java
@@ -1,4 +1,24 @@
 package Lesson_8;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int numbers = 20;
+        int[] arr = new int[numbers];
+        Lesnik lesnik = new Lesnik("D:\\Dropbox\\GeekBrain\\Java_2\\123\\lesnik.txt", numbers);
+        try {
+            arr=lesnik.counter();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=0){
+                System.out.println("Цифра "+i+" встречается "+arr[i]+" раз");
+            }
+        }
+
+    }
 }
+
